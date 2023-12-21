fb6a848acf141d85f9aeb106882d592781324855
adesegun.adebayo <dadebayo.adesegun@gmail.com>
2023-12-21 15:53:06 +0100

    ensure migration process is completed end-to-end for the initial migration file generated

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { User } from 'src/core/entities/user.entity';
+import { InitialMigration1702227594288 } from 'src/migrations/1702227594288-initial_migration';
 import { DataSourceOptions } from 'typeorm';
 
 const sslOption: boolean = process.env.DATABASE_SSL === 'true';
@@ -12,7 +13,7 @@ const databaseConfig: DataSourceOptions = {
   database: process.env.DATABASE_NAME,
   ssl: sslOption,
   entities: [User],
-  migrations: [],
+  migrations: [InitialMigration1702227594288],
   subscribers: [__dirname + '/**/*.subscriber{.ts,.js}'],
   synchronize: false, // must always be false in production
   logging: true,
