0aa74ba6bc7b048fe441302ffe6bf14a5e142037
adesegun.adebayo <dadebayo.adesegun@gmail.com>
2025-05-16 17:30:02 +0100

    chore(fix-test): fix issue with test setup

--- src/admin/admin.service.spec.ts
+++ src/admin/admin.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Permission } from '../core/entities/permission.entity';
+import { UserRole } from '../core/entities/user-role.entity';
+import { RolesPermission } from '../core/entities/roles-permission.entity';
 
 describe('AdminService', () => {
   let service: AdminService;
@@ -9,8 +13,22 @@ describe('AdminService', () => {
       providers: [
         AdminService,
         {
-          provide: 'PermissionRepository',
-          useValue: {}, // Mock implementation of PermissionRepository
+          provide: getRepositoryToken(Permission),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(UserRole),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(RolesPermission),
+          useValue: {
+            find: jest.fn(),
+          },
         },
       ],
     }).compile();
